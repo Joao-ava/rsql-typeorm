e6ff8fa20ce8e856818d5374256a826e8ac80703
Almeida <joao.almeida@gsd.com.br>
2023-10-02 16:23:08 -0300

    fix: check if field is null

--- src/adapt.ts
+++ src/adapt.ts
@@ -25,7 +25,8 @@ import {
   Not,
   And,
   FindOperator,
-  InstanceChecker
+  InstanceChecker,
+  IsNull
 } from 'typeorm';
 
 const mergeArray = <T extends Record<string, any>>(array: T[]): T => {
@@ -62,13 +63,31 @@ const handleEqual = <T>(expression: ComparisonNode): FindOptionsWhere<T>[] => {
   const value = expression.right.value as string;
   const isNotLike = !value.startsWith('*') && !value.endsWith('*');
   if (isNotLike) {
-    return [{ [selectorKey]: Equal(value) }] as FindOptionsWhere<T>[];
+    const finalValue = value === 'NULL' ? IsNull() : Equal(value);
+    return [{ [selectorKey]: finalValue }] as FindOptionsWhere<T>[];
   }
   const leftValue = value.startsWith('*') ? `%${value.slice(1)}` : value;
   const finalValue = value.endsWith('*')
     ? `${leftValue.slice(0, -1)}%`
     : leftValue;
   return [{ [selectorKey]: ILike(finalValue) }] as FindOptionsWhere<T>[];
+};
+
+const handleNotEqual = <T>(
+  expression: ComparisonNode
+): FindOptionsWhere<T>[] => {
+  const selectorKey = (expression as ComparisonNode).left.selector;
+  const value = expression.right.value as string;
+  const isNotLike = !value.startsWith('*') && !value.endsWith('*');
+  if (isNotLike) {
+    const finalValue = value === 'NULL' ? Not(IsNull()) : Not(Equal(value));
+    return [{ [selectorKey]: finalValue }] as FindOptionsWhere<T>[];
+  }
+  const leftValue = value.startsWith('*') ? `%${value.slice(1)}` : value;
+  const finalValue = value.endsWith('*')
+    ? `${leftValue.slice(0, -1)}%`
+    : leftValue;
+  return [{ [selectorKey]: Not(ILike(finalValue)) }] as FindOptionsWhere<T>[];
 };
 
 const getComparisonBySelector = (
@@ -171,6 +190,8 @@ export const adaptRsqlExpressionToQuery = <T>(
   switch (expression.operator) {
     case EQ:
       return handleEqual(expression);
+    case NEQ:
+      return handleNotEqual(expression);
     case GT:
       return [
         { [selectorKey]: MoreThan(getScalarValue(expression.right.value)) }
@@ -190,10 +211,6 @@ export const adaptRsqlExpressionToQuery = <T>(
         {
           [selectorKey]: LessThanOrEqual(getScalarValue(expression.right.value))
         }
-      ] as FindOptionsWhere<T>[];
-    case NEQ:
-      return [
-        { [selectorKey]: Not(expression.right.value) }
       ] as FindOptionsWhere<T>[];
     case IN:
       return [
